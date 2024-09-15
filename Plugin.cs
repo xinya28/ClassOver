4b670ead8439a1c6f4f892803cbaf60acae1d110
xinya28 <140145748+xinya28@users.noreply.github.com>
2024-09-15 16:53:25 +0800

    Update Plugin000.csproj

---
+++ Plugin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassIsland.Core.Abstractions;
+using ClassIsland.Core.Attributes;
+using ClassIsland.Core.Controls.CommonDialog;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Plugin000;
+
+[PluginEntrance]
+public class Plugin : PluginBase
+{
+    public override void Initialize(HostBuilderContext context, IServiceCollection services)
+    {
+        CommonDialog.ShowInfo("Hello world!");
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Plugin000
+# xinya28 的插件模板
+## 这是一个由 xinya28 制作的插件模板
+
+### 插件 id: Plugin.Template
+### 插件面向的 ClassIsland 版本: 1.4.2.0
+### 插件入口程序集entranceAssembly: Plugin000.dll
+### 插件作者: xinya28
+### 插件版本: 0.0.0.1
